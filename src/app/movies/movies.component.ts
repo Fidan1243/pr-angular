cc2b45e091b5fffb79fa8056d881f929f9e4b5dd
Fidan1243 <fidan.imanova.87@gmail.com>
2022-06-13 13:31:26 +0400

    Initial commit

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../models/movie';
+import { MovieRepository } from '../models/movie.repository';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css'],
+})
+export class MoviesComponent implements OnInit {
+  title: string = 'Movie List';
+  movies: Movie[];
+  popularMovies: Movie[];
+  filteredMovies: Movie[];
+  movieRepository: MovieRepository;
+  valueFav: Movie[];
+  filterText: string = '';
+
+  constructor() {
+    this.movieRepository = new MovieRepository();
+    this.movies = this.movieRepository.getMovies();
+    //this.movies.length=0;
+    this.popularMovies = this.movieRepository.getPopularMovies();
+  }
+  ngOnInit() {}
+
+  OnInputChange() {
+    this.filteredMovies = this.filterText
+      ? this.movies.filter(
+          (m: Movie) =>
+            m.title.toLocaleLowerCase().indexOf(this.filterText) !== -1 ||
+            m.description.toLocaleLowerCase().indexOf(this.filterText) !== -1
+        )
+      : this.movies;
+  }
+  Change(item: any, btn: any) {
+    item.isFavorite = !item.isFavorite;
+    if (!item.isFavorite) {
+      btn.value = '🖤';
+    } else {
+      btn.value = '❤️';
+    }
+  }
+  addToList($event: any, item: Movie) {
+    if ($event.target.classList.contains('btn-primary')) {
+      $event.target.innerText = 'Remove List';
+      $event.target.classList.remove('btn-primary');
+      $event.target.classList.add('btn-danger');
+    } else {
+      $event.target.innerText = 'Add To List';
+      $event.target.classList.remove('btn-danger');
+      $event.target.classList.add('btn-primary');
+    }
+  }
+}
